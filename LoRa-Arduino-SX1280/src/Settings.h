93708ca903b404faaa819ed7570543ce56040616
1Blademaster <kush.makkapati@icloud.com>
2023-03-26 04:47:37 +0100

    Format file

--- LoRa-Arduino-SX1280/src/Settings.h
+++ LoRa-Arduino-SX1280/src/Settings.h
@@ -7,10 +7,10 @@
 
 //*******  Setup hardware pin definitions here ! ***************
 
-//These are the pin definitions for one of my own boards, the Easy Pro Mini,
-//be sure to change the definitions to match your own setup. Some pins such as DIO2,
-//DIO3, BUZZER may not be in used by this sketch so they do not need to be
-//connected and should be set to -1.
+// These are the pin definitions for one of my own boards, the Easy Pro Mini,
+// be sure to change the definitions to match your own setup. Some pins such as DIO2,
+// DIO3, BUZZER may not be in used by this sketch so they do not need to be
+// connected and should be set to -1.
 
 #include <stdint.h>
 
@@ -19,23 +19,22 @@
 #define NRESET 9
 #define LED1 13
 #define DIO1 3
-#define DIO2 -1                 //not used 
-#define DIO3 -1                 //not used
-#define RX_EN -1                //pin for RX enable, used on some SX1280 devices, set to -1 if not used
-#define TX_EN -1                //pin for TX enable, used on some SX1280 devices, set to -1 if not used 
+#define DIO2 -1  // not used
+#define DIO3 -1  // not used
+#define RX_EN -1 // pin for RX enable, used on some SX1280 devices, set to -1 if not used
+#define TX_EN -1 // pin for TX enable, used on some SX1280 devices, set to -1 if not used
 
+#define LORA_DEVICE DEVICE_SX1280 // we need to define the device we are using
 
-#define LORA_DEVICE DEVICE_SX1280                //we need to define the device we are using  
+// LoRa Modem Parameters
+const uint32_t Frequency = 2445000000;    // frequency of transmissions
+const int32_t Offset = 0;                 // offset frequency for calibration purposes
+const uint8_t Bandwidth = LORA_BW_0400;   // LoRa bandwidth
+const uint8_t SpreadingFactor = LORA_SF7; // LoRa spreading factor
+const uint8_t CodeRate = LORA_CR_4_5;     // LoRa coding rate
 
-//LoRa Modem Parameters
-const uint32_t Frequency = 2445000000;           //frequency of transmissions
-const int32_t Offset = 0;                        //offset frequency for calibration purposes
-const uint8_t Bandwidth = LORA_BW_0400;          //LoRa bandwidth
-const uint8_t SpreadingFactor = LORA_SF7;        //LoRa spreading factor
-const uint8_t CodeRate = LORA_CR_4_5;            //LoRa coding rate
+const int8_t TXpower = 10; // Power for transmissions in dBm
 
-const int8_t TXpower = 10;                       //Power for transmissions in dBm
+const uint16_t packet_delay = 1000; // mS delay between packets
 
-const uint16_t packet_delay = 1000;              //mS delay between packets
-
-#define RXBUFFER_SIZE 255                        //RX buffer size  
+#define RXBUFFER_SIZE 255 // RX buffer size
