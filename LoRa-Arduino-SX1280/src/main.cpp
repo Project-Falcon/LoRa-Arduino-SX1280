5417796183830ee03784de41371f3011aaa09615
Kush Makkapati <kush.makkapati@icloud.com>
2023-03-28 16:02:30 +0100

    Remove choice for receiving or transmitting

--- LoRa-Arduino-SX1280/src/main.cpp
+++ LoRa-Arduino-SX1280/src/main.cpp
@@ -24,31 +24,31 @@ void transmit()
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("Enter a mode (0=receiving, 1=transmitting): ");
-  while (1)
-  {
-    if (Serial.available())
-    {
-      char input_mode = Serial.read();
-      Serial.println(input_mode);
-      if (input_mode == '0')
-      {
-        Serial.println("\033[0;32mLora set to receiving mode\033[0m");
-        mode = 0; // receiving
-        break;
-      }
-      else if (input_mode == '1')
-      {
-        Serial.println("\033[0;32mLora set to transmitting mode\033[0m");
-        mode = 1; // transmitting
-        break;
-      }
-      else
-      {
-        Serial.println("Enter a mode (0=receiving, 1=transmitting): ");
-      }
-    }
-  }
+  // Serial.print("Enter a mode (0=receiving, 1=transmitting): ");
+  // while (1)
+  // {
+  //   if (Serial.available())
+  //   {
+  //     char input_mode = Serial.read();
+  //     Serial.println(input_mode);
+  //     if (input_mode == '0')
+  //     {
+  //       Serial.println("\033[0;32mLora set to receiving mode\033[0m");
+  //       mode = 0; // receiving
+  //       break;
+  //     }
+  //     else if (input_mode == '1')
+  //     {
+  //       Serial.println("\033[0;32mLora set to transmitting mode\033[0m");
+  //       mode = 1; // transmitting
+  //       break;
+  //     }
+  //     else
+  //     {
+  //       Serial.println("Enter a mode (0=receiving, 1=transmitting): ");
+  //     }
+  //   }
+  // }
   Serial.println();
   myLora.Setup();
   Serial.println();
