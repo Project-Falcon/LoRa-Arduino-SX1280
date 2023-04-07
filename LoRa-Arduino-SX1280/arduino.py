0db00357b4c99137dfa1ceaaeedf1e38bb25703e
tsyrd <93266125+tsyrd@users.noreply.github.com>
2023-04-07 14:09:27 +0100

    Updated getComPort function

--- LoRa-Arduino-SX1280/arduino.py
+++ LoRa-Arduino-SX1280/arduino.py
@@ -2,31 +2,39 @@ import serial
 from serial.tools import list_ports
 import time
 import sys
-from colorama import Fore
-
+from colorama import Fore, Style
 
 def getComPort() -> str:
     while True:
         ports = list(list_ports.comports())
-        port_names = [port.name.lower() for port in ports]
 
-        print(f"{Fore.BLUE}Available COM ports:")
-        for port in ports:
-            print(f"{Fore.BLUE}\t{port.name}: {port.description}")
+        print(f"{Style.BRIGHT}{Fore.BLUE}Available COM ports:")
+        for i in range(len(ports)):
+            port = ports[i]
+            print(f"{Style.BRIGHT}{Fore.BLUE}\t[{i}]\t{port.name}: {port.description}")
 
-        inp_port = input(f"{Fore.YELLOW}Enter a port to connect to: ")
-        if inp_port.lower() in port_names:
+        inp_port = input(f"{Fore.YELLOW}Enter a port index to connect to: ")
+        if not inp_port.isdigit():
+            continue
+
+        inp_port = int(inp_port)
+        if 0 <= inp_port < len(ports):
             break
 
-    print(Fore.RESET)
+    print(Style.RESET_ALL)
 
+    if sys.platform == "darwin":
+        port_name = ports[inp_port].name
+        if port_name[:3] == 'cu.':
+            port_name = port_name[3:]
+            
+        inp_port = f"/dev/tty.{port_name}"
     return inp_port
 
 
 try:
     # create a new serial connection
     ser = serial.Serial(getComPort(), baudrate=9600, timeout=5)
-
     # check if the serial connection is open
     if ser.is_open:
         print("Serial connection is available")
