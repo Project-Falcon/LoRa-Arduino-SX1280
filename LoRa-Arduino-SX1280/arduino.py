8f17f24c2740ddb181891d688193d06f7584a4e2
Kush Makkapati <kush.makkapati@icloud.com>
2023-04-07 15:19:27 +0100

    Add transmission message object and parser

--- LoRa-Arduino-SX1280/arduino.py
+++ LoRa-Arduino-SX1280/arduino.py
@@ -36,6 +36,27 @@ def getComPort() -> str:
     return inp_port
 
 
+class TransmissionMessage:
+    def __init__(self, raw_msg: str):
+        msg_data_split = msg.split(";")
+        msg_data_split = [data.split(":", 1) for data in msg_data_split]
+        msg_data = {data[0]: data[1] for data in msg_data_split}
+
+        self.status = msg_data.get("Status")
+        self.tx_power = msg_data.get("dBm")
+        self.msg = msg_data.get("Packet")
+        if self.msg:
+            self.msg = self.msg[:-1]
+        self.bytes_sent = msg_data.get("BytesSent")
+        self.crc = msg_data.get("CRC")
+        self.transmit_time = msg_data.get("TransmitTime")
+        self.irq_reg = msg_data.get("IRQReg")
+        self.irq_status = msg_data.get("IRQStatus")
+
+    def __repr__(self):
+        return f"[{self.status}] msg: {self.msg}, transmit time: {self.transmit_time}"
+
+
 try:
     # create a new serial connection
     ser = serial.Serial(getComPort(), baudrate=9600, timeout=5)
@@ -49,6 +70,7 @@ except serial.SerialException:
 
 while True:
     if ser.inWaiting() > 0:
-        input = ser.readline()
-        print(input)
+        ser_input = ser.readline().decode("utf-8").strip()
+        t_msg = TransmissionMessage(ser_input)
+        print(t_msg)
         time.sleep(0.5)
